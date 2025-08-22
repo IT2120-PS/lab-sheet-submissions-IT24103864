3086db8258d49674739403f0f35bdbb944f963c1
IT24103864 <IT24103864@my.sliit.lk>
2025-08-22 16:32:02 +0530

    Update IT24103864.R

--- Lab Sheet 04/IT24103864.R
+++ Lab Sheet 04/IT24103864.R
@@ -1,2 +1,34 @@
 setwd("C:\\Users\\IT24103864\\Desktop\\IT24103864")
 data<-read.table("Exercise.txt",header=True,sep=",")
+
+attach(branch_data)
+fix(branch_data)
+
+str(branch_data)
+
+boxplot(Sales_X1,
+        main="Boxplot of Sales",
+        ylab="Sales_X1",
+        outline=TRUE,
+        outpch=8,
+        horizontal=TRUE)
+
+#2
+#Types & scales:
+#Branch: identifier(nominal)
+#Scales_X1:quantitative, ratio
+#Advertising_X2:quantitative,ratio
+#Year_X3:quantitative discrete,ratio 
+quantile(Advertising_X2,
+     probs=c(0,0.25,0.5,0.75,1))
+IQR(Advertising_X2)
+Find_outliers<-function(x){
+  q1<-quantile(x,0.25)
+  q3<-quantile(x,0.75)
+  IQR_value<-q3-q1
+  lower<-q1-1.5*IQR_value
+  upper<-q3+1.5*IQR_value
+  outliers<-x[x<lower|x>upper]
+  return(outliers)
+}
+}    
